fdb532da111d987ca0d250116d46d0b02b22a46a
faisal <faisal25march99@gmail.com>
2021-04-06 20:11:08 +0530

    Finishing up node server project

--- 02WebsiteServer/server.js
+++ 02WebsiteServer/server.js
@@ -3,6 +3,9 @@ const url = require("url");
 const fs = require("fs");
 const path = require("path");
 
+const hostname = "127.0.0.1";
+const port = 5000;
+
 const mimeTypes = {
   html: "text/html",
   css: "text/css",
@@ -11,3 +14,38 @@ const mimeTypes = {
   jpeg: "image/jpeg",
   jpg: "image/jpg",
 };
+
+http
+  .createServer((req, res) => {
+    var myuri = url.parse(req.url).pathname;
+    console.log(myuri);
+    var filename = path.join(process.cwd(), unescape(myuri));
+    console.log("file you are looking for is:" + filename);
+    var loadFile;
+    try {
+      loadFile = fs.lstatSync(filename);
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write("404 Page not found");
+      res.end();
+      return;
+    }
+
+    if (loadFile.isFile()) {
+      var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+      res.writeHead(200, { "Content-Type": mimeType });
+
+      var filestream = fs.createReadStream(filename);
+      filestream.pipe(res);
+    } else if (loadFile.isDirectory()) {
+      res.writeHead(302, { Location: "index.html" });
+      res.end();
+    } else {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.write("500 Internal error");
+      res.end();
+    }
+  })
+  .listen(port, hostname, () => {
+    console.log(`server is running at port: ${port}`);
+  });
