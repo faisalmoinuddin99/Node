635414c79e82706228857385e085cf4afd1e89f4
faisal <faisal25march99@gmail.com>
2021-04-08 13:32:51 +0530

    GET POST and DELETE requests - Postman

--- 03expressBasics/index.js
+++ 03expressBasics/index.js
@@ -8,9 +8,26 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.send("<h1>About Page</h1>");
 });
-// create route for contact-us and services [unordered list]
+
+app.post("/login", (req, res) => {
+  res.send("login success");
+});
+/*
+we get this output: Cannot GET /login
+
+# Because, Browser usually makes a get request
+# For post you need front-end like form's or postman
+
+*/
+
+// Assignment 1: create route for contact-us and services [unordered list]
 app.get("/services", (req, res) => {
   res.send("<ul> <li> coffee </li> <li> Tea </li> <li> Milk </li> </ul>");
+});
+
+// Assignment 2: create a delete route and test it using postman
+app.delete("/del", (req, res) => {
+  res.send("Deleted Successfully...");
 });
 
 app.get("/contact-us", (req, res) => {
