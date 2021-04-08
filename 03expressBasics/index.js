9feb9aee5b1b0dd14b177a000c0defe68a5b5880
faisal <faisal25march99@gmail.com>
2021-04-08 08:09:07 +0530

    Done with Assignment Contact-us and Services Routes

--- 03expressBasics/index.js
+++ 03expressBasics/index.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.send("<h1>About Page</h1>");
 });
+// create route for contact-us and services [unordered list]
+app.get("/services", (req, res) => {
+  res.send("<ul> <li> coffee </li> <li> Tea </li> <li> Milk </li> </ul>");
+});
+
+app.get("/contact-us", (req, res) => {
+  res.send("Contact us Page");
+});
 
 app.listen(3000, () => {
   console.log(`Server is running at port 3000....`);
