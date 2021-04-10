7696ea79a268a6217eeebb52c0a06c72c0005b10
faisal <faisal25march99@gmail.com>
2021-04-10 11:47:49 +0530

    Configuring multer for uploads

--- 07multerupload/index.js
+++ 07multerupload/index.js
@@ -6,14 +6,34 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// multer setting
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/public/myupload");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+var upload = multer({
+  storage: storage,
+}).single("myimage");
+
 // set for ejs
 app.set("view engine", "ejs");
 
-app.use("", express.static(__dirname + "/public"));
+// middleware to set static folder
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  res.send("Multer is going fine and so is nodemon");
+  //res.send("Multer is going fine and so is nodemon");
+  res.render("index");
 });
+
 app.listen(port, () => {
   console.log(`server is running at ${port}...`);
 });
