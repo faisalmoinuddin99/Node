917342fac5c0b32d0684fe079339289468964d5c
faisal <faisal25march99@gmail.com>
2021-04-11 20:59:24 +0530

    Done with Connecting project with mongoDB

--- 09bigstack/app.js
+++ 09bigstack/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+//mongodb configuration
+const db = require("./setup/myurl").mongoURL;
+
+//Attempt to connect to database
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 
---
+++ 09bigstack/setup/myurl.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURL:
+    "mongodb+srv://bigstack-db:root@cluster0.yavff.mongodb.net/bigstack-db?retryWrites=true&w=majority",
+  secret: "mystrongsecret",
+};
