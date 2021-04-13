7fbf4fefde5b8963cb99aba26869390fdda55320
faisal <faisal25march99@gmail.com>
2021-04-13 08:29:20 +0530

    Done with Creating Person Schema

--- 09bigstack/routes/api/questions.js
+++ 09bigstack/routes/api/questions.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
-    test: "Questions success",
+    question: "Questions success",
   });
 });
 module.exports = router;
