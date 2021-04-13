5bac819126d12f90601a80541ebcbced2dd66272
faisal <faisal25march99@gmail.com>
2021-04-13 08:42:16 +0530

    Done with first query in MongoDB

--- 09bigstack/routes/api/auth.js
+++ 09bigstack/routes/api/auth.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jsonwt = require("json-web-token");
+const passport = require("passport");
+const key = require("../../setup/myurl");
 
 //@type     GET
 //@route    /api/auth
 //@desc     just for testing
 //@access   PUBLIC
 router.get("/", (req, res) => {
-  res.json({ test: "Auth is success" });
+  res.json({ test: "Auth is being tested" });
 });
 
+//Import Schema for Person to Register
+const Person = require("../../models/Person");
+
+//@type     POST
+//@route    /api/auth/register
+//@desc     route for registerion of users
+//@access   PUBLIC
+router.post("/register", (req, res) => {
+  Person.findOne({ email: req.body.email })
+    .then()
+    .catch((err) => console.log(err));
+});
 module.exports = router;
